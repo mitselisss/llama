8140cc57b6c311c8860b7278551fde33e63a7750
mitselisss <mitselisss@gmail.com>
2024-10-15 11:45:01 +0300

    deleted line 6

--- fine_tune.py
+++ fine_tune.py
@@ -3,8 +3,6 @@ from datasets import load_dataset
 from trl import SFTTrainer
 from transformers import TrainingArguments, TextStreamer
 
-asdasdasd
-
 max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
 dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
 load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
